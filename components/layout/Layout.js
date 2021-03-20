f34424fe873e82f5e19d5692802617811294f0b9
Agustin <aencaje@gmail.com>
2021-03-20 11:49:19 -0300

    New UI

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -5,7 +5,7 @@ const Layout = props => {
     return (
         <>
             <Head>
-                <title>Product Hunt Firebase y Next.js</title>
+                <title>COVID-Graphs Argentina</title>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
             </Head>
